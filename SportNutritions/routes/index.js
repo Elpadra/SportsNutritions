2a87bf3912b870a7359550b62209438a656cce3b
Elpadra <Elpadraaa@gmail.com>
2022-11-22 18:20:00 +0300

    4.3

--- SportNutritions/routes/index.js
+++ SportNutritions/routes/index.js
@@ -5,16 +5,31 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 /* Страница протеина */
 router.get('/protein', function(req, res, next) {
-    res.send("<h1>Страница протина</h1>")
+    res.render('Nutritions', {
+        title: "Протеин",
+        picture: "images/protein.jpg",
+        desc: "Протеин (белок)– это спортивная добавка, что состоит из концентрата натуральных белков."
+    })
 });
 /* Страница бета-аланина */
 router.get('/beta-alanine', function(req, res, next) {
-    res.send("<h1>Страница бета-аланина</h1>")
+    res.render('Nutritions', {
+        title: "Бета-аланин",
+        picture: "images/beta-alanine.jpg",
+        desc: "Бета-аланин - биологически активная добавка к пище, дополнительный источник витамина D3, фолиевой кислоты, изофлавонов, содержащая бета-аланин."
+    })
 });
-/* Страница бца */
+
+
+/* Страница аминокислоты */
 router.get('/bcaa', function(req, res, next) {
-    res.send("<h1>Страница бца</h1>")
+    res.render('Nutritions', {
+        title: "Аминокислота",
+        picture: "images/bcaa.jpg",
+        desc: "BCAA – основной материал для построения новых мышц. Эти аминокислоты составляют 35% всей мышечной массы и принимают важное участие в процессах анаболизма и восстановления, обладают антикатаболическим действием."
+    })
 });
 module.exports = router;
